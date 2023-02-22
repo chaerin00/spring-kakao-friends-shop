0dde16341c5dc346ae2f23251d213470577bdbc0
Anna An <anna.an@freedgrouptech.com>
2023-02-22 15:58:50 +0900

    feat: add product entitiy

--- src/main/java/com/kakao/api/domain/product/api/ProductApi.java
+++ src/main/java/com/kakao/api/domain/product/api/ProductApi.java
@@ -1,15 +1,18 @@
 package com.kakao.api.domain.product.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/product")
 public class ProductApi {
     @GetMapping()
     public String getProduct(){
+        return String.format("product");
+    }
+
+    @PostMapping()
+    public String createProduct(){
+        // TODO: post API 구현
         return String.format("product");
     }
 }
