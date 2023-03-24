d1c4b76c1bf9b6e8bb052f06ce8b30326ee5f587
CHAERIN AN <72637095+chaerin00@users.noreply.github.com>
2023-03-24 17:01:11 +0900

    feature/post-api (#1)
    * wip
    
    * feat: implement create api
    
    * fix: fix drop table issue
    
    * chore: remove unused import

--- src/main/java/com/kakao/api/domain/model/Product.java
+++ src/main/java/com/kakao/api/domain/model/Product.java
@@ -1,23 +1,24 @@
 package com.kakao.api.domain.model;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.UUID;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @Entity
 @Table(name="product")
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id",unique = true)
-    private Long id;
-
+    private String id=UUID.randomUUID().toString();
     @Column(length = 50)
     private String name;
 
     @Column(nullable = false)
     private Double price;
 }
+
--- src/main/java/com/kakao/api/domain/product/api/ProductApi.java
+++ src/main/java/com/kakao/api/domain/product/api/ProductApi.java
@@ -1,18 +1,32 @@
 package com.kakao.api.domain.product.api;
 
+import com.kakao.api.domain.model.Product;
+import com.kakao.api.domain.model.ProductCreationRequest;
+import com.kakao.api.domain.product.application.ProductRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping(value = "/product")
 public class ProductApi {
+    private ProductRepository productRep;
+
+    @Autowired
+    public ProductApi(ProductRepository boardRep) {
+        this.productRep = boardRep;
+    }
+
     @GetMapping()
     public String getProduct(){
         return String.format("product");
     }
 
     @PostMapping()
-    public String createProduct(){
-        // TODO: post API 구현
-        return String.format("product");
+    public Product createProduct(@RequestBody ProductCreationRequest request){
+        Product member = new Product();
+        BeanUtils.copyProperties(request, member);
+        return productRep.save(member);
     }
 }
